ec2b614f0a93a1361fd9b4f922e62e03b73e6005
Soonil Hong <s.hong.5181@gmail.com>
2021-10-30 19:53:02 -0400

    Extract MyHomePage to separate file

---
+++ lib/my_home_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // Fields in a Widget subclass are always marked "final".
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _convert() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text(widget.title)),
+      ),
+      body: Container(
+        // decoration: const BoxDecoration(
+        //   image: DecorationImage(
+        //     image: AssetImage('particles-1200.jpg'),
+        //     // image: AssetImage('building.jpg'),
+        //     fit: BoxFit.cover,
+        //   ),
+        // ),
+        child: Center(
+          child: Column(
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _convert,
+        tooltip: 'Convert',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
